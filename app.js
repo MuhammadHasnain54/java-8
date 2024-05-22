9f701c27c509abd35d623cafb8c0668103b6af50
Muhammad Hasnain <154057614+MuhammadHasnain54@users.noreply.github.com>
2024-05-22 19:44:23 +0500

    java 7

---
+++ app.js
@@ -0,0 +1,36 @@
+document.write("<center>")
+var userNumber = prompt("Enter a number to draw a pattern");
+for (var i = 0; i <= userNumber; i++) {
+    for (var j = 1; j <= i; j++) {
+        document.write("&nbsp;");
+        document.write(userNumber);
+    }
+    document.write("<br>")
+}
+
+
+
+document.write("<br>")
+
+for(var i=10; i>= 1; i--){
+    for(var j=1; j<= i; j++){
+        document.write("&nbsp;");
+        document.write("*");
+    }
+    document.write("<br>");
+}
+
+
+
+document.write("<br>")
+document.write("<br>")
+document.write("<br>")
+document.write("<br>")
+document.write("<br>")
+
+
+for(var i= 7; i>=1; i--){
+    for(var j=1; j<=i; j++){
+       document.write("&nbsp;");
+        document.write("*")
+    }}
